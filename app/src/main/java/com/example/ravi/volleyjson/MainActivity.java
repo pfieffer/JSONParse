77fe319d5ac579217f695853c37f12e2a091a1b1
pfieffer <cbpmgr@gmail.com>
2017-03-25 21:55:25 +0545

    first commit

---
+++ app/src/main/java/com/example/ravi/volleyjson/MainActivity.java
@@ -0,0 +1,188 @@
+package com.example.ravi.volleyjson;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.ravi.volleyjson.app.AppController;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    //json object response url
+    private String urlJsonObj = "http://api.androidhive.info/volley/person_object.json";
+
+    //json array response url
+    private String urlJsonArry = "http://api.androidhive.info/volley/person_array.json";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+    //progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+
+    //temporary string to show the parsed response
+    private String jsonResponse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnMakeObjectRequest = (Button) findViewById(R.id.btnObjRequest);
+        btnMakeArrayRequest = (Button) findViewById(R.id.btnArrayRequest);
+        txtResponse = (TextView) findViewById(R.id.txtResponse);
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please Wait....");
+        pDialog.setCancelable(false);
+
+        btnMakeObjectRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //making json object request
+                makeJsonObjectRequest(); //function definition below
+            }
+        });
+        
+        btnMakeArrayRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //maing json array Request
+                makeJsonArrayRequest(); //function definition below
+            }
+        });
+    }
+
+    private void makeJsonObjectRequest() {
+
+        showpDialog(); //showing progress dialog while json oject is being fetched
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                urlJsonObj,
+                null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json object response
+                            // response will be a json object
+                            String name = response.getString("name");
+                            String email = response.getString("email");
+                            JSONObject phone = response.getJSONObject("phone");
+                            String home = phone.getString("home");
+                            String mobile = phone.getString("mobile");
+
+                            jsonResponse = "";
+                            jsonResponse += "Name: " + name + "\n\n";
+                            jsonResponse += "Email: " + email + "\n\n";
+                            jsonResponse += "Home: " + home + "\n\n";
+                            jsonResponse += "Mobile: " + mobile + "\n\n";
+
+                            txtResponse.setText(jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        hidepDialog(); //hide the dialog after the JSON object is loaded
+
+                    }
+
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                VolleyLog.d(TAG,"Error: "+ volleyError.getMessage() );
+                Toast.makeText(getApplicationContext(), volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog(); //hide the progress dialog
+            }
+        });
+
+        //adding request to request queue
+        AppController.getmInstance().addToRequestQueue(jsonObjReq);
+    }
+
+    private void makeJsonArrayRequest() {
+
+        showpDialog(); //show progress dialog while the json is loading
+
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG,response.toString());
+
+                        try{
+                            //parsing json raay response
+                            //loop through each json object
+                            jsonResponse = "";
+                            for(int i = 0; i<response.length();i++){
+                                JSONObject person = (JSONObject) response.get(i);
+                                String name = person.getString("name");
+                                String email = person.getString("email");
+                                JSONObject phone = person.getJSONObject("phone");
+                                String home = phone.getString("home");
+                                String mobile = phone.getString("mobile");
+
+                                jsonResponse += "Name: " + name + "\n\n";
+                                jsonResponse += "Email: " + email + "\n\n";
+                                jsonResponse += "Home: " + home + "\n\n";
+                                jsonResponse += "Mobile: " + mobile + "\n\n\n";
+
+                        }
+                        txtResponse.setText(jsonResponse);
+
+                    } catch(JSONException e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),"Error: "+e.getMessage(),Toast.LENGTH_LONG).show();
+                        }
+                        hidepDialog();
+                }
+    }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                VolleyLog.d(TAG, "Error: " + volleyError.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        AppController.getmInstance().addToRequestQueue(req);
+}
+
+    private void showpDialog(){
+        if(!pDialog.isShowing()){
+            pDialog.show();
+        }
+    }
+
+    private void hidepDialog(){
+        if(pDialog.isShowing()){
+            pDialog.dismiss();
+        }
+    }
+}
